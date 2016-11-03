228426b237a3049273a2aef8f4e20641d4111d45
Ryan.K <ryankung@ieee.org>
2016-11-03 18:44:32 +0800

    created requirements file

--- tests/io/test_schema.py
+++ tests/io/test_schema.py
@@ -1,10 +1,14 @@
+import pytest
 from qubit.io.postgres import connection as conn
 from schema.utils import execute_file
 
 
-def test_create_table():
+@pytest.fixture
+def drop_table():
     file_path = 'schema/drop.sql'
     execute_file(file_path, conn)
 
+
+def test_create_table(drop_table):
     file_path = 'schema/schema.sql'
     execute_file(file_path, conn)
