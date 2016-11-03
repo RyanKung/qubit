913d04e30d9593fac667b1c83bbd7bc7934e58f3
Ryan.K <ryankung@ieee.org>
2016-11-03 11:55:12 +0800

    updated config

--- qubit/config.py
+++ qubit/config.py
@@ -2,4 +2,8 @@ __all__ = ['PGSQL_PARAM']
 
 PGSQL_PARAM = dict(user='ryan',
                    host='127.0.0.1',
+                   database='qubit',
+                   settings={
+                       'search_path': 'qubit',
+                   },
                    port=5432)
